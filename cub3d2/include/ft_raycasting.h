90d7f8c7c444b29459394bb686a5e8811d181ade
Mathias BAXMANN <mbaxmann@e3r7p2.42.fr>
2020-08-01 10:49:37 +0200

    Preparing raysting function

--- cub3d2/include/ft_raycasting.h
+++ cub3d2/include/ft_raycasting.h
@@ -1,41 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   ft_raycasting.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbaxmann <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/14 09:27:20 by mbaxmann          #+#    #+#             */
-/*   Updated: 2020/08/01 10:48:23 by mbaxmann         ###   ########.fr       */
+/*   Created: 2020/08/01 10:23:29 by mbaxmann          #+#    #+#             */
+/*   Updated: 2020/08/01 10:27:48 by mbaxmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#ifndef FT_RAYCASTING_H
+# define FT_RAYCASTING_H
 
-#define t_dim t_coord
-#define A write(1, "A", 1);
-#define B write(1, "B", 1);
-
-typedef struct	s_dim
+typedef struct		s_player
 {
-	int			x;
-	int			y;
-}				t_dim;
-
-typedef	struct	s_mlx
-{
-	void	*ptr;
-	void	*win;
-}				t_mlx;
-
-#include "../mlx/mlx.h"
-#include "../libft/libft.h"
-#include "ft_list.h"
-#include "init.h"
-#include "ft_event.h"
-#include "ft_raycasting.h"
-#include <fcntl.h>
-#include <stdio.h>
+	double			x;
+	double			y;
+	double			angle;
+}					t_player;
 
 #endif
--- cub3d2/srcs/ft_raycasting.c
+++ cub3d2/srcs/ft_raycasting.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_event.c                                         :+:      :+:    :+:   */
+/*   ft_raycasting.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbaxmann <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/01 09:55:23 by mbaxmann          #+#    #+#             */
-/*   Updated: 2020/08/01 10:19:02 by mbaxmann         ###   ########.fr       */
+/*   Created: 2020/08/01 10:18:48 by mbaxmann          #+#    #+#             */
+/*   Updated: 2020/08/01 10:45:03 by mbaxmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int		ft_button_event(int keycode, void *param)
+int			ft_raycasting(t_data *data, t_mlx *mlx)
 {
-	if (keycode == 53)
-	{
-		mlx_destroy_window(((t_mlx *)param)->ptr, ((t_mlx *)param)->win);
-		exit(0);
-	}
-	return (1);
-}
-
-int		ft_close(void *param)
-{
-	mlx_destroy_window(((t_mlx *)param)->ptr, ((t_mlx *)param)->win);
-	exit(0);
-	return (1);
-}
-
-void	ft_event(t_mlx *mlx)
-{
-	mlx_hook(mlx->win, 2, 1L << 0, &ft_button_event, mlx);
-	mlx_hook(mlx->win, 17, 1L << 17, &ft_close, mlx);
+	return (0);
 }
