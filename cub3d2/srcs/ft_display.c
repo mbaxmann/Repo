5746c2ffb96f7514a91f6a3923821994532a6b3a
mbaxmann <mathias.bamann4@gmail.com>
2020-08-21 11:51:34 +0200

    finished texturing wall

--- cub3d2/srcs/ft_display.c
+++ cub3d2/srcs/ft_display.c
@@ -42,6 +42,20 @@ int		ft_choose_textur(t_data *data)
 	}
 }
 
+void		ft_calculate_xpm_offset(int *tab, t_data *data, int size)
+{
+	int	i;
+	int	j;
+
+	i = (tab[0] * data->texture[tab[2]]->height) / size;
+	if (data->stock->x)
+		j = (int)round(data->stock->x) % CUBE;
+	else
+		j = (int)round(data->stock->y) % CUBE;
+	j = (j * data->texture[tab[2]]->width) / CUBE;
+	tab[1] = 4 * (data->texture[tab[2]]->width * i + j);
+}
+
 void		ft_put_pixel(char *img, char *xpm, int i, int j)
 {
 	*(unsigned int *)(img + i) = *(unsigned int *)(xpm + j);
@@ -50,21 +64,28 @@ void		ft_put_pixel(char *img, char *xpm, int i, int j)
 void		ft_load_ray(t_data *data, int size, int offset)
 {
 	int 		tab[4];
-	unsigned int	rgb;
+	int		i;
 	long long	offset_2;
 
 	tab[0] = 0;
-	tab[1] = size;
+	tab[1] = 0;
+	i = 0;
 	offset_2 = 0;
-	size = (size > data->res->y) ? data->res->y - 1: size;
-	offset_2 = (offset + 1) * 4 + data->res->x * 4 * ((data->res->y - size) / 2);
+	if (size > data->res->y)
+	{
+		tab[0] = (size - data->res->y) / 2;
+		size -= 2 * tab[0];
+	}
+	offset_2 = offset * 4 + data->res->x * 4 * ((data->res->y - size) / 2);
 	tab[2] = ft_choose_textur(data);
 	tab[3] = 0;
-	while (tab[0] < size)
+	i = tab[0];
+	size += 2 * tab[0];
+	while (tab[0] < size - i)
 	{
-		ft_put_rgb(data->image->pt, data->texture[tab[2]], offsett_2, tab[1]);
+		ft_put_pixel(data->img->pt, data->texture[tab[2]]->addr, offset_2, tab[1]);
 		offset_2 += data->res->x * 4;
-		tab[1] = 
+		ft_calculate_xpm_offset(tab, data, size);
 		tab[0]++;
 	}
 }
